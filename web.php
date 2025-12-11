ec65ad3b2310fc163fb5e74682fd2fff16e36d12
Eaindra <eaindrakyaw887@gmail.com>
2025-12-11 14:15:46 +0630

    Add files via upload

---
+++ web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/products', function () {
+ return 'Products List';
+});
+Route::get('/products/names', function () {
+ return 'Romand';
+});
+
+Route::get('/products/names/{id}', function ( $id ) {
+ return "Romand - $id";
+});
+
+
+Route::get('/products/names', function () {
+ return 'Romand';
+})->name('products.names');
+
+Route::get('/products/more', function() {
+ return redirect('/products/names');
+});
+
+Route::get('/products/more', function() {
+ return redirect()->route('products.detail');
+});
