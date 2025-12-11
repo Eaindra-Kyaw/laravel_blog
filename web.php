31623c265243e06a3055bfb598e1ace90cf8e25d
Eaindra <eaindrakyaw887@gmail.com>
2025-12-11 14:18:45 +0630

    Day 4-web.php

--- web.php
+++ web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', function () {
- return 'Products List';
+ return 'Lipsticks';
 });
 Route::get('/products/names', function () {
  return 'Romand';
