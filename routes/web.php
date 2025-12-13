88c34896dae6348d23a8fb338dcb82e9f63e2126
Eaindra <eaindrakyaw887@gmail.com>
2025-12-13 19:07:17 +0630

    second commit

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', function () {
- return 'Products List';
-});
-Route::get('/products/names', function () {
- return 'Romand';
-});
+use App\Http\Controllers\Product\ArticleController;
 
-Route::get('/products/names/{id}', function ( $id ) {
- return "Romand - $id";
-});
+Route::get('/index1', [ArticleController::class, 'index1']);
+Route::get('/sample', [ArticleController::class, 'sample']);
+Route::get('/detail/{id}', [ArticleController::class, 'detail']);
+Route::get('/articles', [ArticleController::class, 'index']); // for your $data array
 
 
-Route::get('/products/names', function () {
- return 'Romand';
-})->name('products.names');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/products/more', function() {
- return redirect('/products/names');
-});
+// Route::get('/products', function () {
+//     return 'Lipsticks';
+// });
 
-Route::get('/products/more', function() {
- return redirect()->route('products.detail');
-});
+// Route::get('/products/names', function () {
+//     return 'Romand';
+// });
+
+// Route::get('/products/names/{id}', function ($id) {
+//     return "Romand - $id";
+// });
+
+// Route::get('/products/names', function () {
+//     return 'Romand';
+// })->name('products.names');
+
+// Route::get('/products/more', function () {
+//     return redirect('/products/names');
+// });
+
+// Route::get('/products/more', function () {
+//     return redirect()->route('products.names');
+// });
