500c1886545a32f4b9f596262e4f750889d23221
Eaindra <eaindrakyaw887@gmail.com>
2025-12-15 20:14:17 +0630

    2025_12_15_081004_create_categories_table.php

--- database/migrations/2025_12_15_081004_create_categories_table.php
+++ database/migrations/2025_12_15_081004_create_categories_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
 {
     Schema::create('categories', function (Blueprint $table) {
     $table->id();
-    $table->string('name');  // ← MUST have
-    $table->text('description')->nullable();  // ← MUST have
+    $table->string('name'); 
+    $table->text('description')->nullable();  
     $table->timestamps();
 });
 }
