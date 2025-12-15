cd1ae4fa477bc82fcbb305c2689a6465fcaa7e2b
Eaindra <eaindrakyaw887@gmail.com>
2025-12-15 15:19:15 +0630

    third commit

---
+++ 2025_12_15_081004_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+ public function up(): void
+{
+    Schema::create('products', function (Blueprint $table) {
+    $table->id();
+    $table->string('name');  // ← MUST have
+    $table->text('description')->nullable();  // ← THIS IS MISSING!
+    $table->decimal('price', 8, 2);  // ← MUST have
+    $table->integer('stock')->default(0);  // ← MUST have
+    $table->foreignId('category_id')->constrained()->onDelete('cascade');  // ← MUST have
+    $table->timestamps();
+});
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
