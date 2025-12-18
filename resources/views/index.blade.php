0dc661325f95941f7cb7625d096da1a90619c86d
Eaindra <eaindrakyaw887@gmail.com>
2025-12-18 15:24:48 +0630

    forth commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html>
 
 <head>
-    <meta charset="utf-8">
-    <title>Product List</title>
+    <title>Employee List</title>
 </head>
 
 <body>
-    <h1>Product List</h1>
+
+    <h1>Employee List</h1>
+
+    @if (session('success'))
+        <p style="color:green;">{{ session('success') }}</p>
+    @endif
+
     <ul>
-        <?php foreach($articles as $article): ?>
-        <li><?php echo $article['item']; ?></li>
-        <?php endforeach ?>
+        @foreach ($employees as $employee)
+            <li>
+                {{ $employee->id }} | {{ $employee->name }} | Age: {{ $employee->age }} | Position:
+                {{ $employee->position }}
+            </li>
+        @endforeach
     </ul>
+
+    <p>
+        {{-- <a href="/create-employee">Create Sample Employee</a> | --}}
+        {{-- <a href="/update-employee">Update Employee ID 2</a> | --}}
+        <a href="/delete-employee">Delete Employee ID 2</a>
+    </p>
+
 </body>
 
 </html>
